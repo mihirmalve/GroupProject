2da716d7039180658dbc0ca0dad0f984b7140c4b
Ravi Teja <ravitejakarnati04@gmail.com>
2025-08-16 16:40:47 +0530

    checks

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import GroupController from "../controllers/groupController.js";
 
 const router = express.Router();
+
 router.post("/create", GroupController.createGroup);
 router.post("/join", GroupController.joinGroup);
 router.post("/info", GroupController.getGroupInfo);
